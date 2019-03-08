9997aee88121930b0ffeb776c6e329154a90fcd7
Ryan Gale <ryanwfgale@gmail.com>
2019-03-08 16:46:19 -0700

    add JS to card and include form

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 $(document).ready( () => {
 let allCards = [
-  { front: "This is the front of the card", back: "this is the back of the card" },
-  { front: "This is the front of the card 2", back: "this is the back of the card 2" },
+  { front: "What is let?", back: "let gives you access to declared variables that are limited in scope to the block, statement of expression unlike var. In other words, let variables are scoped to the innermost curly brackets { }." },
+  { front: "What is const?", back: "Const is another variable declaration that will be used to replace var. const is short for constant meaning these values CAN'T change. const is great because it removes the 'magic number' pattern that we often fall into as developers." },
   { front: "This is the front of the card 3", back: "this is the back of the card 3" }
 ]
 
+
+
+$(document).on('click', '.create', () => {
+    event.preventDefault();
+    front = document.getElementById('new-front').value
+    back = document.getElementById('new-back').value
+        console.log(allCards)
+
+    allCards.push({front, back})
+    console.log(allCards)
+    //debugger
+  })
 
 $(document).on('click', '.show-card', () => {
   event.preventDefault();
